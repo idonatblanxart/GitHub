870fe2c203d95348bb9c1075fdab7e7bc939f88f
super <super@daw>
2024-03-11 16:55:41 +0100

    Nombre añadido

--- provaGit/src/provaGit/EjemploGit.java
+++ provaGit/src/provaGit/EjemploGit.java
@@ -1,3 +1,11 @@
 package provaGit;
 
-public class EjemploGit    
+public class EjemploGit {
+	
+	public static void main(String[] args) {
+		//TODO Autho-generated method stub
+		System.out.println("Hola");
+		
+		String nombre ="Juan";
+	}
+}
